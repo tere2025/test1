d8345245648cf82adfa96c5a1c2db444c3f615ea
tthkmaks <tthkmaks@gmail.com>
2024-12-26 14:13:30 +0200

    new_test2

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/test.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/test.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+762d43425679d193cb0f9e081bf55fb6e595a6b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+addf579ad99400467e742f28809de9d5c36467b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
